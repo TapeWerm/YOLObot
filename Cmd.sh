e6ed4a7872e0c26ad0f9174a3765b41a490563d2
TapeWerm <TapeWerm@users.noreply.github.com>
2020-05-23 16:22:37 -0700

    Appease shellcheck

--- Cmd.sh
+++ Cmd.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 read -r Nick user chan msg
-# $0 is the path
-dir=$(dirname "$0")
 # Decapitalizes uppercase letters
-nick=$(echo "$Nick" | tr [:upper:] [:lower:])
+nick=$(echo "$Nick" | tr '[:upper:]' '[:lower:]')
 
 reply() {
 	echo YOLO
@@ -13,10 +11,10 @@ reply() {
 action() {
 	case "$1 $2" in
 	# `/me pets YOLObot for scaring students` moves closing Start of Header
-	`echo -e "pets $nick\001"`|"pets $nick")
+	$(echo -e "pets $nick\001")|"pets $nick")
 		echo -e "\001ACTION pets $user\001"
 		;;
-	`echo -e "pats $nick\001"`|"pats $nick")
+	$(echo -e "pats $nick\001")|"pats $nick")
 		echo -e "\001ACTION pats $user\001"
 		;;
 	esac
@@ -45,21 +43,21 @@ msg=$(echo "$msg" | sed -E s/^--\?// | sed -E s/\ --\?/\ /)
 if [ "$chan" = "$Nick" ]; then
 	# No trigger if you're messaging
 	# Case statements are only case sensitive if parameters are passed by script
-	reply `echo $msg | tr [:upper:] [:lower:]`
+	reply "$(echo "$msg" | tr '[:upper:]' '[:lower:]')"
 # YOLObot doesn't take orders from other bots
 # WermBot ACRONYMbot werm_bot werm-bot
 # Bots with hard-to-parse nicks that prefix a trigger
 # Names can end in bot and people can be voiced in #bots too
 elif echo "$user" | grep -Evq "Bot\b|[A-Z][A-Z]bot\b|[_-]bot\b|ExampleSpambot"; then
-	cmd=$(echo "$msg" | cut -d ' ' -f 1 | tr [:upper:] [:lower:])
+	cmd=$(echo "$msg" | cut -d ' ' -f 1 | tr '[:upper:]' '[:lower:]')
 	case $cmd in
 	$nick:|@$nick|$nick|+$nick|!$nick)
 		# Without -s the first string is counted if there isn't a 2nd
-		reply $(echo "$msg" | cut -d ' ' -f 2- -s | tr [:upper:] [:lower:])
+		reply "$(echo "$msg" | cut -d ' ' -f 2- -s | tr '[:upper:]' '[:lower:]')"
 		;;
 	# /me pets YOLObot
 	$(echo -e "\001action"))
-		action $(echo "$msg" | cut -d ' ' -f 2-3 -s | tr [:upper:] [:lower:])
+		action "$(echo "$msg" | cut -d ' ' -f 2-3 -s | tr '[:upper:]' '[:lower:]')"
 		;;
 	esac
 fi
